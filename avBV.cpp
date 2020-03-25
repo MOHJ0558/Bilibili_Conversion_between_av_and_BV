015b1cb34bb5ab7ab205810b4dbfc0c19d0b7b17
Mo Haojun <3155611183@qq.com>
2020-03-25 17:23:20 +0800

    Update avBV.cpp

--- avBV.cpp
+++ avBV.cpp
@@ -10,7 +10,7 @@ const string TABLE = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF
 long long int tr[123] = {};
 long long int dec(string x) {
 	long long int r = 0;
-	for(long long int i = 0; i < 6; i++) {
+	for (long long int i = 0; i < 6; i++) {
 		r += ((long long int)pow(58, i) * tr[x[s[i]]]);
 	}
 	return (r - add) ^ xorn;
@@ -18,7 +18,7 @@ long long int dec(string x) {
 string enc(long long int x) {
 	x = (x ^ xorn) + add;
 	string r = "BV1  4 1 7  ";
-	for(long long int i = 0; i < 6; i++) {
+	for (long long int i = 0; i < 6; i++) {
 		r[s[i]] = TABLE[(long long int)floor(x / (long long int)pow(58, i)) % 58];
 	}
 	return r;
@@ -28,43 +28,45 @@ int main() {
 		tr[TABLE[i]] = i;
 	}
 	start:
-	int ch = 0;
-	cout << "Please choose operation:\n1.av to BV\n2.BV to av\n";
+	string ch;
+	cout << "Please choose operation:\n1.av to BV\n2.BV to av\nYour choice?\n> ";
 	cin >> ch;
-	if(ch == 1) {
-		cout << "Please enter av number: av";
+	if (ch == "1") {
+		cout << "Please enter av number> av";
 		long long int av;
 		cin >> av;
-		cout << enc(av) << endl;
-		cout << "Whether to export to BV.txt?[y/n] ";
-		char isCopy;
+		cout << "Result:\n" << enc(av) << endl;
+		cout << "Whether to export it to BV.txt?(\"y\" for yes/\"n\" for no)\n> ";
+		string isCopy;
 		cin >> isCopy;
-		if(isCopy == 'y' || isCopy == 'Y') {
+		if (isCopy == "y" || isCopy == "Y") {
 			ofstream saveBV("BV.txt");
-			saveBV << "av" << av << " -> " << enc(av);
+			saveBV << "av" << av << " is converted to:\n" << enc(av);
 			saveBV.close();
+			cout << "Success!" << endl;
 		}
-	} else if(ch == 2) {
-		cout << "Please enter BV string, including the letters \"BV\": ";
+	} else if (ch == "2") {
+		cout << "Please enter BV string, including the letters \"BV\"\n> ";
 		string BV;
 		cin >> BV;
-		cout << "av" << dec(BV) << endl;
-		cout << "Whether to export to av.txt?[y/n] ";
-		char isCopy;
+		cout << "Result:\n" <<  "av" << dec(BV) << endl;
+		cout << "Whether to export it to av.txt?(\"y\" for yes/\"n\" for no)\n> ";
+		string isCopy;
 		cin >> isCopy;
-		if(isCopy == 'y' || isCopy == 'Y') {
+		if (isCopy == "y" || isCopy == "Y") {
 			ofstream saveBV("av.txt");
-			saveBV << BV << " -> av" << dec(BV);
+			saveBV << BV << " is converted to:\n" << dec(BV);
 			saveBV.close();
+			cout << "Success!" << endl;
 		}
 	} else {
 		cerr << "ERROR!" << endl;
-		return -1;
+		goto start;
 	}
-	cout << "continue?[y/n] ";
-	char con;
+	cout << "Continue?(\"y\" for yes/\"n\" for no)\n> ";
+	string con;
 	cin >> con;
-	if (con == 'y' || con == 'Y') {
+	if (con == "y" || con == "Y") {
 		goto start;
 	}
 	return 0;
