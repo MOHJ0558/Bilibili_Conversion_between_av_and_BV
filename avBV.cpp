fa719590bba8323eb699457f73af886ec8506d23
Mo Haojun <3155611183@qq.com>
2020-03-25 09:05:49 +0800

    改动源码结构和输出界面，增加重头再来标识

--- avBV.cpp
+++ avBV.cpp
@@ -1,45 +1,42 @@
 #include <iostream>
 #include <math.h>
+#include <string>
 using namespace std;
 long long int xorn = 177451812;
 long long int add = 8728348608;
-int s[] = {11,10,3,8,4,6};
+int s[] = {11, 10, 3, 8, 4, 6};
+const string TABLE = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+long long int tr[123] = {};
 long long int dec(string x) {
-    string table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-    long long int tr[123] = {};
-    for (long long int i = 0; i < 58; i++) {
-        tr[table[i]]=i;
-    }
-    long long int r = 0;
-    for(long long int i = 0; i < 6; i++) {
-        r += ((long long int)pow(58, i) * tr[x[s[i]]]);
-    }
-    return (r - add) ^ xorn;
+	long long int r = 0;
+	for(long long int i = 0; i < 6; i++) {
+		r += ((long long int)pow(58, i) * tr[x[s[i]]]);
+	}
+	return (r - add) ^ xorn;
 }
 string enc(long long int x) {
-	string table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-    char tr[123] = {};
-    for (long long int i = 0; i < 58; i++) {
-        tr[table[i]]=i;
-    }
-    x = (x ^ xorn) + add;
-    string r = "BV1  4 1 7  ";
-    for(long long int i = 0; i < 6; i++) {
-        r[s[i]] = table[(long long int)floor(x/(long long int)pow(58, i))%58];
-    }
-    return r;
+	x = (x ^ xorn) + add;
+	string r = "BV1  4 1 7  ";
+	for(long long int i = 0; i < 6; i++) {
+		r[s[i]] = TABLE[(long long int)floor(x / (long long int)pow(58, i)) % 58];
+	}
+	return r;
 }
 int main() {
+	for (long long int i = 0; i < 58; i++) {
+		tr[TABLE[i]] = i;
+	}
+	start:
 	int ch = 0;
-	cout << "Please choose operation:\n1.av2BV\n2.BV2av\n";
+	cout << "Please choose operation:\n1.av to BV\n2.BV to av\n";
 	cin >> ch;
 	if(ch == 1) {
-		cout << "Please input av number, except \"av\":";
+		cout << "Please enter av number, not including the letters \"av\":";
 		long long int av;
 		cin >> av;
 		cout << enc(av) << endl;
 	} else if(ch == 2) {
-		cout << "Please input BV string, ay attention to uppercase and lowercase:";
+		cout << "Please enter BV string, including the letters \"BV\":";
 		string BV;
 		cin >> BV;
 		cout << "av" << dec(BV) << endl;
@@ -47,5 +44,11 @@ int main() {
 		cerr << "ERROR!" << endl;
 		return -1;
 	}
-    return 0;
+	cout << "continue?[y/n] ";
+	string con;
+	cin >> con;
+	if (con == "y" || con == "Y") {
+		goto start;
+	}
+	return 0;
 }
