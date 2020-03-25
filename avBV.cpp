104be39de7096e07bf550a1e6d6f4488db051ace
Mo Haojun <3155611183@qq.com>
2020-03-25 11:25:59 +0800

    改善交互界面，增加导出到文件

--- avBV.cpp
+++ avBV.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <fstream>
 using namespace std;
 long long int xorn = 177451812;
 long long int add = 8728348608;
@@ -31,23 +32,39 @@ int main() {
 	cout << "Please choose operation:\n1.av to BV\n2.BV to av\n";
 	cin >> ch;
 	if(ch == 1) {
-		cout << "Please enter av number, not including the letters \"av\":";
+		cout << "Please enter av number: av";
 		long long int av;
 		cin >> av;
 		cout << enc(av) << endl;
+		cout << "Whether to export to BV.txt?[y/n] ";
+		char isCopy;
+		cin >> isCopy;
+		if(isCopy == 'y' || isCopy == 'Y') {
+			ofstream saveBV("BV.txt");
+			saveBV << "av" << av << " -> " << enc(av);
+			saveBV.close();
+		}
 	} else if(ch == 2) {
-		cout << "Please enter BV string, including the letters \"BV\":";
+		cout << "Please enter BV string, including the letters \"BV\": ";
 		string BV;
 		cin >> BV;
 		cout << "av" << dec(BV) << endl;
+		cout << "Whether to export to av.txt?[y/n] ";
+		char isCopy;
+		cin >> isCopy;
+		if(isCopy == 'y' || isCopy == 'Y') {
+			ofstream saveBV("av.txt");
+			saveBV << BV << " -> av" << dec(BV);
+			saveBV.close();
+		}
 	} else {
 		cerr << "ERROR!" << endl;
 		return -1;
 	}
 	cout << "continue?[y/n] ";
-	string con;
+	char con;
 	cin >> con;
-	if (con == "y" || con == "Y") {
+	if (con == 'y' || con == 'Y') {
 		goto start;
 	}
 	return 0;
